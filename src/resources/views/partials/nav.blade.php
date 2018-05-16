73cd14f3515bf4811a7ce2a0d6734d5e05997076
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2018-05-15 17:03:35 -0800

    Continue bs4 update

--- src/resources/views/partials/nav.blade.php
+++ src/resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-custom">
-        <div class="collapse navbar-collapse container" id="app-navbar-collapse">
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
@@ -10,7 +10,7 @@
 
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
+                <li class="nav-item active">
                     <a href="{{ url('/') }}" class="nav-link">Home</a>
                 </li>
             </ul>
@@ -22,28 +22,26 @@
                     <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                 @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <i class="fa fa-fw fa-user"></i>
                             {{ Auth::user()->name }}
                             @if( Auth::user()->isAdmin() )
-                            <span class="label label-info">Admin</span>
+                            <span class="badge badge-info">Admin</span>
                             @endif
-                            <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                     Logout
                                 </a>
                             </li>
 
-                            <li>
-
-                                <a href="#"
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"
                                     onclick="event.preventDefault();
                                              Bus.$emit('ShowPasswordForm')">
                                     Reset Password
@@ -55,7 +53,15 @@
                             </li>
                         </ul>
                     </li>
+
+
+
                 @endif
             </ul>
+            @auth
+                <button onclick="Bus.$emit('ToggleRight')" :class="{active : showRightMenu, animate : animateChat}" class="btn btn-info btn-sm ml-2">
+                    <i class="fa fa-fw fa-bars"></i>
+                </button>
+            @endauth
         </div>
 </nav>
